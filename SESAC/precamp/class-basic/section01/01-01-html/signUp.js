81d0ccc7754562b4f8d475ec77a6479038d32ad3
minjeong <130037564+minjeong9707@users.noreply.github.com>
2024-08-28 12:47:22 +0900

    sesac precamp 공부 및 실습 추가

---
+++ SESAC/precamp/class-basic/section01/01-01-html/signUp.js
@@ -0,0 +1,20 @@
+function registerFunc() {
+  const email = document.getElementById('email').value;
+
+  //이메일 * 표시
+  const dash = email.split('@');
+  // const authEmail = dash[0];
+  //dash[0]의 끝에서부터 4개 *
+  const fourString = dash[0].slice(-4);
+  const headString = dash[0].slice(0, -5);
+  console.log(headString);
+
+  const authEmail_1 = fourString.length;
+  console.log(authEmail_1);
+  const changeEmail = '*'.repeat(authEmail_1);
+  console.log(changeEmail);
+
+  const result_email = changeEmail + '@' + dash[1];
+  alert(`회원가입을 축하합니다. 가입하신 이메일은 ${result_email}입니다.`);
+  // 나중에 버튼 누르면 초기화되눈거
+}
---
+++ SESAC/precamp/h.w/signupSlack.js
@@ -0,0 +1,158 @@
+function registerFunc() {
+  const memberName = document.getElementById('memberName').value;
+  const emailInfo = document.getElementById('emailInfo').value;
+  const pw = document.getElementById('pw').value;
+  const confirmPW = document.getElementById('confirmPW').value;
+  const phone = document.getElementById('phone').value;
+  const userInfo = document.getElementById('userInfo').value;
+  const gender = document.querySelector('input[name="gender"]:checked');
+  const agree = document.querySelector('input[id="info"]:checked');
+
+  const todayDate = new Date();
+  let year = todayDate.getFullYear();
+  let month = String(todayDate.getMonth() + 1).padStart(2, '0');
+  let day = String(todayDate.getDay()).padStart(2, '0');
+
+  //마지막에 다시 주석제거
+  if (
+    memberName.length === 0 ||
+    emailInfo.length === 0 ||
+    pw.length === 0 ||
+    confirmPW.length === 0 ||
+    phone.length === 0 ||
+    userInfo.length === 0 ||
+    gender === null ||
+    !agree
+  ) {
+    alert('모든 값을 입력해주세요.');
+    return;
+  }
+
+  // 비밀번호 확인이 일치하지 않는지 확인
+  if (pw !== confirmPW) {
+    alert('비밀번호가 일치하지 않습니다.');
+    return;
+  }
+
+  //성별 값 추출
+  const selectGender = gender.value;
+
+  //회원정보 객체 저장
+  const userInfoList = {
+    name: memberName,
+    email: emailInfo,
+    password: pw,
+    phone: phone,
+    userInfo: userInfo,
+    gender: selectGender,
+    agree: agree.checked,
+    registrationDate: `${year}-${month}-${day}`,
+  };
+  console.log(userInfoList);
+
+  displayMember(userInfoList);
+
+  alert(`
+  ${memberName}님, 회원가입을 축하합니다.
+  (가입일시 : ${year}-${month}-${day})`);
+
+  // 모든 입력 필드를 초기화
+  document.getElementById('memberName').value = '';
+  document.getElementById('emailInfo').value = '';
+  document.getElementById('pw').value = '';
+  document.getElementById('confirmPW').value = '';
+  document.getElementById('phone').value = '';
+  document.getElementById('userInfo').value = '';
+
+  // 체크박스와 라디오 버튼 선택 해제
+  document.querySelector('input[name="gender"]:checked').checked = false;
+  document.getElementById('info').checked = false;
+}
+//회원추가한거 leftmenu에 보이게 하는 함수
+function displayMember(userInfo) {
+  const memberInfoDiv = document.querySelector('.memberInfo');
+
+  //전화번호 가운데 ***
+  const phoneNum = userInfo.phone;
+  const dash = phoneNum.split('-');
+  const newPhone = dash[0] + '-' + '****' + '-' + dash[2];
+
+  //비밀번호 ****표시
+  const pw = userInfo.password;
+  const changePwStr = pw.toString();
+  const length = changePwStr.length;
+  const changePw = '*'.repeat(length);
+
+  memberInfoDiv.innerHTML = '';
+  const memberListDiv = document.createElement('div');
+  memberListDiv.className = 'member-list';
+  memberListDiv.innerHTML = `
+  <img src = "./img/circleProfile.png">
+  <p>${userInfo.name}</p>`;
+
+  memberListDiv.addEventListener('click', function () {
+    alert(`
+  이름 : ${userInfo.name}
+  이메일 : ${userInfo.email}
+  비밀번호 : ${changePw}
+  성별 : ${userInfo.gender}
+  전화번호 : ${newPhone}
+  동의여부 : ${userInfo.agree ? 'Y' : 'N'}
+  자기소개 : ${userInfo.userInfo}
+  (가입일시 : ${userInfo.registrationDate})`);
+  });
+
+  memberInfoDiv.appendChild(memberListDiv);
+}
+
+function requestAuthNum() {
+  let time = 180;
+  const randomNum = String(Math.floor(Math.random() * 1000000)).padStart(
+    6,
+    '0'
+  );
+  document.getElementById('authNum').innerText = randomNum;
+
+  const verifyBtn = document.getElementById('verifyBtn');
+  verifyBtn.disabled = false;
+  verifyBtn.innerText = '인증 하기';
+  verifyBtn.style.backgroundColor = ' #491449';
+
+  document.getElementById('timer').style.display = 'block';
+
+  updateTimer(time);
+
+  const intervalId = setInterval(function () {
+    time = time - 1;
+    updateTimer(time);
+    if (time <= 0) {
+      clearInterval(intervalId);
+      disableVerifyButton();
+    }
+  }, 1000);
+}
+function disableVerifyButton() {
+  const verifyBtn = document.getElementById('verifyBtn');
+  verifyBtn.disabled = true;
+  verifyBtn.style.backgroundColor = '#C7C7C7';
+  verifyBtn.style.color = '#F2F2F2';
+}
+function updateTimer(time) {
+  const minute = Math.floor(time / 60);
+  const second = String(time % 60).padStart(2, '0');
+  document.getElementById('timer').innerHTML = `<div>${minute}:${second}</div>`;
+}
+
+function authFunc() {
+  const verifyBtn = document.getElementById('verifyBtn');
+
+  if (!verifyBtn.disabled) {
+    // 버튼이 비활성화되지 않았을 때만 실행
+    verifyBtn.disabled = true;
+    verifyBtn.style.backgroundColor = '#C7C7C7';
+    // verifyBtn.style.color = '#F2F2F2';
+    verifyBtn.innerText = '인증 완료';
+
+    document.getElementById('timer').style.display = 'none';
+  }
+}
