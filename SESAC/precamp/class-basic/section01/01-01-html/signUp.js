30424bbd086bfda3451cd8242bc288e3229dd627
minjeong <130037564+minjeong9707@users.noreply.github.com>
2024-08-29 01:51:04 +0900

    section3 필수 과제 수행

--- SESAC/precamp/class-basic/section01/01-01-html/signUp.js
+++ SESAC/precamp/class-basic/section01/01-01-html/signUp.js
@@ -3,7 +3,7 @@ function registerFunc() {
 
   //이메일 * 표시
   const dash = email.split('@');
-  // const authEmail = dash[0];
+  const authEmail = dash[0];
   //dash[0]의 끝에서부터 4개 *
   const fourString = dash[0].slice(-4);
   const headString = dash[0].slice(0, -5);
@@ -14,7 +14,8 @@ function registerFunc() {
   const changeEmail = '*'.repeat(authEmail_1);
   console.log(changeEmail);
 
-  const result_email = changeEmail + '@' + dash[1];
+  const result_email = authEmail + changeEmail + '@' + dash[1];
   alert(`회원가입을 축하합니다. 가입하신 이메일은 ${result_email}입니다.`);
   // 나중에 버튼 누르면 초기화되눈거
+  // 모든 값을 입력해야 버튼 실행되는건 안함
 }
