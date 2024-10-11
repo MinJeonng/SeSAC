21c535b5bda88aaa1cd163270d694a20623e890c
minjeong <130037564+minjeong9707@users.noreply.github.com>
2024-10-11 15:39:01 +0900

    feat : layout 컴포넌트

--- Day16-55_mainCamp/class-example/my-app/src/commons/layout/header/index.tsx
+++ Day16-55_mainCamp/class-example/my-app/src/commons/layout/header/index.tsx
@@ -1,4 +1,4 @@
 'use client';
 export default function Header() {
-  return <div className="h-[30px] bg-red-400">해더입니다.</div>;
+  return <div className="h-[30px] bg-red-400">헤더입니다.</div>;
 }
--- Day16-55_mainCamp/class-example/my-app/src/commons/settings/06-02-apollo-setting.tsx
+++ Day16-55_mainCamp/class-example/my-app/src/commons/settings/06-02-apollo-setting.tsx
@@ -7,7 +7,6 @@ interface IApolloSetting {
 }
 export default function ApolloSetting(props: IApolloSetting) {
   const client = new ApolloClient({
-    // 과제할땐 example이 아니라 practice로 바뀌어야함
     uri: 'http://main-example.codebootcamp.co.kr/graphql',
     cache: new InMemoryCache(),
   });
