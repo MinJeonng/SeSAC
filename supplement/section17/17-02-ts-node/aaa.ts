7a55930f44afff2b1e0df4737846ba9c28a61ac4
minjeong <130037564+minjeong9707@users.noreply.github.com>
2024-10-17 15:31:22 +0900

    feat : node, decorator 관련 파일 생성 및 공부

---
+++ supplement/section17/17-02-ts-node/aaa.ts
@@ -0,0 +1,4 @@
+console.log('노드 확인');
+
+const apple: number = 3;
+console.log(apple);
---
+++ supplement/section17/17-03-decorator/decorator-ex.ts
@@ -0,0 +1,10 @@
+function bbb(data: any) {
+  console.log('========');
+  console.log(data);
+  console.log('========');
+}
+@bbb
+class User {
+  //id : number
+  //firstName : string
+}
