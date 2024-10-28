6b07d36e7ede3a7651da083f87e68aadd0cf59b2
minjeong <130037564+minjeong9707@users.noreply.github.com>
2024-10-28 17:34:09 +0900

    feat : global state로 로그인 에러 해결

--- Day16-55_mainCamp/class-practice/my-app/src/app/section22/22-01-login/page.tsx
+++ Day16-55_mainCamp/class-practice/my-app/src/app/section22/22-01-login/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { useAccessTokenStore } from '@/commons/stores/22-01-accessToken-store';
 import { gql, useMutation } from '@apollo/client';
-import { Modal } from 'antd';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
@@ -25,34 +24,33 @@ export default function LoginPage() {
   const onClickLogin = async () => {
     const { email, password } = formState;
     //1. login 뮤테이션 날려서 token 받아오기
-    const result = await loginUser({
-      variables: {
-        email: email,
-        password: password,
-      },
-      // 여기선 회원가입이 없으니까 일단 값 일시적으로 넣어주기
-      // variables: {
-      //   email: '33@naver.com',
-      //   password: '1234',
-      // },
-    });
-    console.log(result, '로그인 결과');
-    const accessToken = result.data.loginUser.accessToken;
 
-    //2. 받아온 token을 globalState에 저장하기
-    if (accessToken === undefined) {
-      alert('로그인 실패');
-      return;
+    console.log('result 하고');
+    try {
+      const result = await loginUser({
+        variables: { email, password },
+        // 여기선 회원가입이 없으니까 일단 값 일시적으로 넣어주기
+        // variables: {
+        //   email: '33@naver.com',
+        //   password: '1234',
+        // },
+      });
+      console.log(result, '로그인 결과');
+
+      if (result?.data?.loginUser?.accessToken) {
+        const accessToken = result.data.loginUser.accessToken;
+        // 2. 받아온 token을 globalstate에 저장하기
+        setAccessToken(accessToken); // globalState에 token set하기
+        console.log(accessToken, 'token 값');
+        //3. 성공페이지 이동
+        router.push('/section22/22-01-login-success');
+      } else {
+        console.log('토큰없음. 로그인실패');
+        alert('로그인 실패');
+      }
+    } catch (error) {
+      console.log('로그인 중 오류 발생', error);
     }
-    setAccessToken(accessToken); // globalState에 token set하기
-    console.log(accessToken, 'token 값');
-
-    //3. 성공페이지 이동
-    Modal.success({
-      title: '성공',
-      content: '로그인 성공',
-    });
-    router.push('/section22/22-01-login-success');
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -61,7 +59,7 @@ export default function LoginPage() {
   };
   return (
     <>
-      이메일 : <input type="text" name="email " onChange={handleChange} />
+      이메일 : <input type="text" name="email" onChange={handleChange} />
       비밀번호 :
       <input type="password" name="password" onChange={handleChange} />
       <button onClick={onClickLogin}>로그인</button>
